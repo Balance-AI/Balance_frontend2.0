c6e96b9a96afd93ad5faf7b9adec52391b997ec2
Mahdi <mahditanzim2@gmail.com>
2024-06-11 14:07:08 -0400

    [new component] : Supported.js / Supported.module.css

---
+++ components/Supported.js
@@ -0,0 +1,9 @@
+import supportedstyles from '../styles/Supported.module.css'
+
+export default function Supported() {
+    return (
+        <>
+        This is supported component
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Supported from "@/components/Supported";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Supported />
     </>
   );
 }
