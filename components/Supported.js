1471bbba27c646163285822a806789fa889dcffc
MahdiT54 <mahditanzim2@gmail.com>
2024-06-12 13:09:47 -0400

    support hovers updated

--- components/Supported.js
+++ components/Supported.js
@@ -20,19 +20,23 @@ export default function Supported() {
           <h3>Centralize All Your Financial & Marketing Data</h3>
           <h2 className={supportedstyles.supported__h2}>We Support</h2>
           <div className={supportedstyles.supported__logos}>
-            <figure className={supportedstyles.supported__logo}>
+            <figure
+              className={`${supportedstyles.supported__logo} bai__button--hover`}
+            >
               Youtube <FontAwesomeIcon icon={faYoutube} />
             </figure>
-            <figure className={supportedstyles.supported__logo}>
+            <figure
+              className={`${supportedstyles.supported__logo} bai__button--hover`}
+            >
               Meta <FontAwesomeIcon icon={faMeta} />
             </figure>
-            <figure className={supportedstyles.supported__logo}>
+            <figure className={`${supportedstyles.supported__logo} bai__button--hover`}>
               Tiktok <FontAwesomeIcon icon={faTiktok} />
             </figure>
-            <figure className={supportedstyles.supported__logo}>
+            <figure className={`${supportedstyles.supported__logo} bai__button--hover`}>
               Google <FontAwesomeIcon icon={faGoogle} />
             </figure>
-            <figure className={supportedstyles.supported__logo}>
+            <figure className={`${supportedstyles.supported__logo} bai__button--hover`}>
               Shopify <FontAwesomeIcon icon={faShopify} />
             </figure>
           </div>
