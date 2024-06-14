e9378a0cef8f30fcb38c7f5667bbf0ff2d644e60
Mahdi <mahditanzim2@gmail.com>
2024-06-14 16:44:32 -0400

    analytics component in prog

--- components/Analytics.js
+++ components/Analytics.js
@@ -3,20 +3,22 @@ import analyticsstyles from "../styles/Analytics.module.css";
 export default function Analytics() {
   return (
     <div className="row">
-      <div className="container">
-        <h1 style={{ textAlign: "center" }}>Intuitive Analytics Dashboard</h1>
-        <figure style={{ width: "calc(100% / 3)" }}>
-          <div>Insert img here</div>
-          Financial Metrics
-        </figure>
-        <figure>
-          <div>Insert img here</div>
-          Marketing Metrics
-        </figure>
-        <figure>
-          <div>Insert img here</div>
-          AI Projected Revenue Forecast
-        </figure>
+      <div className={`${analyticsstyles.analytics__container} container`} >
+        <h1 className={analyticsstyles.analytics__h1}>Intuitive Analytics Dashboard</h1>
+        <div className={analyticsstyles.dashboard__wrap}>
+          <figure className={analyticsstyles["analytics__img--wrap"]}>
+            <img src="/analyticsimg1.svg" alt="Description of Image" /> AI
+            Projected Revenue Forecast Financial Metrics
+          </figure>
+          <figure className={analyticsstyles["analytics__img--wrap"]}>
+            <img src="/analyticsimg2.png" alt="Description of Image" /> AI
+            Marketing Metrics
+          </figure>
+          <figure className={analyticsstyles["analytics__img--wrap"]}>
+            <img src="/analyticsimg3.png" alt="Description of Image" /> AI
+            Projected Revenue Forecast
+          </figure>
+        </div>
       </div>
     </div>
   );
