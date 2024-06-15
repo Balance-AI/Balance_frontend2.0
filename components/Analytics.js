8bbc2779aa19d6b26b2328a816ebd0df0d0a98e5
MahdiT54 <mahditanzim2@gmail.com>
2024-06-15 09:54:23 -0400

    phase 1

--- components/Analytics.js
+++ components/Analytics.js
@@ -3,23 +3,22 @@ import analyticsstyles from "../styles/Analytics.module.css";
 export default function Analytics() {
   return (
     <div className="row">
-      <div className={`${analyticsstyles.analytics__container} container`} >
-        <h1 className={analyticsstyles.analytics__h1}>Intuitive Analytics Dashboard</h1>
+      <div className={`${analyticsstyles.analytics__container} container`}>
+        <h1 className={analyticsstyles.analytics__h1}>
+          Intuitive Analytics Dashboard
+        </h1>
         <div className={analyticsstyles.dashboard__wrap}>
           <figure className={analyticsstyles["analytics__img--wrap"]}>
-            <img src="/analyticsimg1.svg" alt="Description of Image" /> AI
-            Projected Revenue Forecast Financial Metrics
+            <img src="/analyticsimg1.svg" alt="Description of Image" />
           </figure>
           <figure className={analyticsstyles["analytics__img--wrap"]}>
-            <img src="/analyticsimg2.svg" alt="Description of Image" /> AI
-            Marketing Metrics
+            <img src="/analyticsimg2.svg" alt="Description of Image" />
           </figure>
           <figure className={analyticsstyles["analytics__img--wrap"]}>
-            <img src="/analyticsimg3.svg" alt="Description of Image" /> AI
-            Projected Revenue Forecast
+            <img src="/analyticsimg3.svg" alt="Description of Image" />
           </figure>
         </div>
       </div>
     </div>
-  );
+  )
 }
