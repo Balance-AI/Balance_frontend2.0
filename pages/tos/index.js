93171c87cbda206c8b6b5ac66fb12ef27931ead9
Tahmidur_Rabb <tr1476@nyu.edu>
2024-08-21 00:06:30 -0400

    New branch

---
+++ pages/tos/index.js
@@ -0,0 +1,22 @@
+// import tosstyles from "../styles/TOS.module.css";
+
+export default function tos() {
+    return(
+    <> 
+    <div> 
+        <ol>
+           <h1>  Terms of Service </h1> 
+           <li>
+               <h1> Acceptance of Terms  </h1>
+           </li>
+           <li> 
+           By accessing or using our Services, you affirm that you are at least 18 years old, or if you are under 18, you have obtained parental or legal guardian consent to use our Services. You agree to comply with these Terms and any applicable laws, regulations, and industry standards.
+           </li>
+        </ol>
+
+    </div>
+
+    </> 
+    
+    )
+}
