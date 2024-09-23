76be4fa3734aa097f59c484850f4e16cee1fab57
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2024-09-23 14:43:19 -0400

    about page video bg

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,15 @@
+import aboutstyles from "../../styles/About.module.css";
+
 export default function About() {
   return (
-    <section className="uplift">
+    <section className={`${aboutstyles.about__header} `}>
+      <video
+        src="https://cdn.vidzflow.com/v/H0LDXZjsnq_1080p_1703000142.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className={aboutstyles["hero__video--lighting"]}></div>
       <div className="row">
         <div className="container">
           <h1>About Us</h1>
