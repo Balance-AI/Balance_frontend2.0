fbdb1dfbdefce41523701b1c1cf2d3057bcf88fc
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2024-12-23 03:42:11 -0500

    .

--- components/Landing/ProdInfo.js
+++ components/Landing/ProdInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Prodstyles from "../../styles/Prodstyles.module.css";
+import Image from "next/image";
 
 export default function Prodinfo() {
   return (
@@ -10,11 +11,19 @@ export default function Prodinfo() {
         <div className={Prodstyles.prodinfo__container}>
           <div className={Prodstyles.prodinfo__buttons}>
             <button className={Prodstyles.prodinfo__button}>AI</button>
-            <button className={Prodstyles.prodinfo__button}>AI</button>
-            <button className={Prodstyles.prodinfo__button}>AI</button>
-            <button className={Prodstyles.prodinfo__button}>AI</button>
-            <button className={Prodstyles.prodinfo__button}>AI</button>
-            <button className={Prodstyles.prodinfo__button}>AI</button>
+            <button className={Prodstyles.prodinfo__button}>Governance</button>
+            <button className={Prodstyles.prodinfo__button}>Warehousing</button>
+            <button className={Prodstyles.prodinfo__button}>ETL</button>
+            <button className={Prodstyles.prodinfo__button}>Data Sharing</button>
+            <button className={Prodstyles.prodinfo__button}>Orchestration</button>
+          </div>
+          <div className={Prodstyles.prodinfo__frontboard}>
+            <div className={Prodstyles.prodinfo__frontboard__left}>
+                Left
+            </div>
+            <div className={Prodstyles.prodinfo__frontboard__right}>
+                <Image src={"/productscreen-static-ai.png"} width={500} height={500}/>
+            </div>
           </div>
         </div>
         {/* <div className={Prodstyles.prodinfo__lefthalf}>
