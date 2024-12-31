3ae76ddc110a49273dc9a3052f634d102c269833
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2024-12-30 21:02:42 -0500

    ip

--- components/Landing/Platform.js
+++ components/Landing/Platform.js
@@ -5,9 +5,19 @@ const Platform = () => {
   return (
     <section className={platformstyles.platform}>
       <div className={"row"}>
-        <div className={"container"}>
-          <span className={platformstyles.platform__use}>Use Cases</span>
-          <h2 className={platformstyles.platform__h2}>Centralize All Your Data + AI</h2>
+        <div className={`${platformstyles.platform__container} container`}>
+          <span className={platformstyles.platform__use}>Platform</span>
+          <h2 className={platformstyles.platform__h2}>
+            The BalanceAI Data Intelligence Platform
+          </h2>
+          <span className={platformstyles.platform__databring}>
+            BalanceAI brings AI to your data to help you bring AI to the world
+          </span>
+          <div className={platformstyles.platform__boxes}>
+            <div className={platformstyles.platform__box}>Boxie 1</div>
+            <div className={platformstyles.platform__box}>Boxie 1</div>
+            <div className={platformstyles.platform__box}>Boxie 1</div>
+          </div>
         </div>
       </div>
     </section>
